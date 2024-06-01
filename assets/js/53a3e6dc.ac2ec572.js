7d00dacdc184d5b42ec45ba5e90a3da155eb292e
franklai <franklai@users.noreply.github.com>
2024-06-01 00:24:12 +0000

    deploy: 9fe291365b6b7ee3b1502f828aae2d40de8c5a5b

---
+++ assets/js/53a3e6dc.ac2ec572.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkfranks543_website=self.webpackChunkfranks543_website||[]).push([[6261],{922:e=>{e.exports=JSON.parse('{"metadata":{"permalink":"/blog","page":1,"postsPerPage":3,"totalPages":4,"totalCount":12,"nextPage":"/blog/page/2","blogDescription":"Blog","blogTitle":"Blog"}}')}}]);
